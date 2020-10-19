e66ed1ad187dfbee2bffe2e1aab34cd10ed3e0f4
hahalala2020 <72992690+hahalala2020@users.noreply.github.com>
2020-10-20 01:52:41 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
-sudo pip install uwsgi flask
+#安装 python
+sudo apt-get update
 sudo apt-get install python-pip python-dev nginx
+#安装 Flask 和 uwsgi:
+sudo pip install uwsgi flask
+#安装nginx
+sudo apt-get install nginx
+#安装 supervisor
+sudo apt-get install supervisor
+#安装 mysql
+sudo apt-get install mysql-server
